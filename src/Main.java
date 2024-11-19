cbe6a55d5a64f2b0b9e7a9e63980a5c37112c202
KimTaeYoun <147122070+codenamecloud@users.noreply.github.com>
2024-11-19 16:25:15 +0900

    완성

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //rock = 0, paper = 1, scissors = 2
+        Integer com = random.nextInt(3);
+
+        String answer = "Yes";
+        while(answer.equals("Yes") || answer.equals("Y") || answer.equals("yes") || answer.equals("y")) {
+            System.out.println("가위바위보!");
+            System.out.print("무엇을 내시겠습니까? ");
+            String me = scanner.nextLine();
+            if(me.equals("가위") && com.equals(0)) {
+                System.out.println("com: 주먹");
+                System.out.println("졌습니다 ㅠㅠ");
+            } else if(me.equals("가위") && com.equals(1)) {
+                System.out.println("com: 보");
+                System.out.println("이겼습니다!!");
+            } else if(me.equals("가위") && com.equals(2)) {
+                System.out.println("com: 가위");
+                System.out.println("비겼습니다.");
+            } else if(me.equals("바위") && com.equals(0)) {
+                System.out.println("com: 주먹");
+                System.out.println("비겼습니다.");
+            } else if(me.equals("바위") && com.equals(1)) {
+                System.out.println("com: 보");
+                System.out.println("졌습니다 ㅠㅠ");
+            } else if(me.equals("바위") && com.equals(2)) {
+                System.out.println("com: 가위");
+                System.out.println("이겼습니다!!");
+            } else if(me.equals("보") && com.equals(0)) {
+                System.out.println("com: 주먹");
+                System.out.println("이겼습니다!!");
+            } else if(me.equals("보") && com.equals(1)) {
+                System.out.println("com: 보");
+                System.out.println("비겼습니다");
+            } else if(me.equals("보") && com.equals(2)) {
+                System.out.println("com: 가위");
+                System.out.println("졌습니다 ㅠㅠ");
+            }
+            System.out.print("계속하시겠습니까?(Yes/No): ");
+            answer = scanner.nextLine();
         }
+
     }
 }
